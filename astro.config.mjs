58d8be19f0e74aff7022e42747084beea3f9020d
theo <me@t3.gg>
2022-04-13 16:37:36 -0700

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  integrations: [tailwind()]
 });
